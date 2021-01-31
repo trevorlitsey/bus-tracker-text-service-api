7b967f6d1d7082241fbbf2174d337b1cc847b779
Trevor Litsey <trevorlitsey@gmail.com>
2021-01-31 15:03:01 -0600

    fix cors

--- src/main/java/com/trevorlitsey/textbustrackerapi/TextBusTrackerApplication.java
+++ src/main/java/com/trevorlitsey/textbustrackerapi/TextBusTrackerApplication.java
@@ -3,10 +3,7 @@ package com.trevorlitsey.textbustrackerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableMongoAuditing
@@ -17,15 +14,5 @@ public class TextBusTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TextBusTrackerApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
 	}
 }
---
+++ src/main/java/com/trevorlitsey/textbustrackerapi/utils/MvcConfigurer.java
@@ -0,0 +1,16 @@
+package com.trevorlitsey.textbustrackerapi.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
