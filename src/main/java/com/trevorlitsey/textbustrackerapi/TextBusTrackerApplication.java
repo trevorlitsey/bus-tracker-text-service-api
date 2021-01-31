4755e9bdcf7d87fe13b979406ce5a549aa94e0fa
Trevor Litsey <trevorlitsey@gmail.com>
2021-01-31 14:52:07 -0600

    add cors

--- src/main/java/com/trevorlitsey/textbustrackerapi/TextBusTrackerApplication.java
+++ src/main/java/com/trevorlitsey/textbustrackerapi/TextBusTrackerApplication.java
@@ -3,7 +3,10 @@ package com.trevorlitsey.textbustrackerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableMongoAuditing
@@ -16,4 +19,13 @@ public class TextBusTrackerApplication {
 		SpringApplication.run(TextBusTrackerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
