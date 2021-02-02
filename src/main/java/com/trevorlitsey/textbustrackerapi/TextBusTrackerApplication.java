38945ff2ecb05f2582006844eb7ec9dc0f140733
Trevor Litsey <trevorlitsey@gmail.com>
2021-02-01 20:51:20 -0600

    cross!

--- src/main/java/com/trevorlitsey/textbustrackerapi/TextBusTrackerApplication.java
+++ src/main/java/com/trevorlitsey/textbustrackerapi/TextBusTrackerApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableMongoAuditing
 @SpringBootApplication
 @EnableSwagger2
 @EnableFeignClients
+@CrossOrigin(origins = "*")
 public class TextBusTrackerApplication {
 
 	public static void main(String[] args) {
